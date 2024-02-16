f3046ac1de73808dcf3f465228510c8749e9b7f6
Alex <alexmora140500@gmail.com>
2024-02-16 09:34:01 -0300

    final2 Commit

--- Backend/Models/Local-file-System/TourModel.js
+++ Backend/Models/Local-file-System/TourModel.js
@@ -2,46 +2,54 @@
 import { randomUUID } from "node:crypto"
 import { readJSON } from "../../Serv.js"
 
-const Tours = readJSON("./Tours.json")
+let Tours;
 
 export class TourModel {
-    static async getAll ({location}) {
-    if (location) {
-    return Tours.filter(
-        Tours => Tours.location.some(l => l.toLowerCase()=== location.toLowerCase())
-        )
+    static async init() {
+        Tours = readJSON("./Tours.json");
     }
-    return Tours
+
+    static async getAll({ location }) {
+        if (location) {
+            return Tours.filter(Tour => Tour.location.some(l => l.toLowerCase() === location.toLowerCase()));
+        }
+        return Tours;
     }
-    static async getById ({id}) {
-        const Tour = Tours.find(Tour => Tour.id === id)
-        return Tour
+
+    static async getById({ id }) {
+        const Tour = Tours.find(Tour => Tour.id === id);
+        return Tour;
     }
-    static async create  ({input}) {
-        const newTours ={
+
+    static async create({ input }) {
+        const newTours = {
             id: randomUUID(),
             ...input
-        }
-        Tours.push(newTours)
-        return newTours
+        };
+        Tours.push(newTours);
+        return newTours;
     }
-    static async delete ({id}) {
-        const ToursIndex = Tours.findIndex(Tour => Tour.id === id)
-        if (ToursIndex === -1) return false
 
-        Tours.splice(ToursIndex, 1)
+    static async delete({ id }) {
+        const ToursIndex = Tours.findIndex(Tour => Tour.id === id);
+        if (ToursIndex === -1) return false;
+
+        Tours.splice(ToursIndex,  1);
         return true;
     }
-    static async update({id, input }) {
-        const ToursIndex = Tours.findIndex(Tour => Tour.id === id)
-        if (ToursIndex === -1) return false 
+
+    static async update({ id, input }) {
+        const ToursIndex = Tours.findIndex(Tour => Tour.id === id);
+        if (ToursIndex === -1) return false;
 
         Tours[ToursIndex] = {
             ...Tours[ToursIndex],
             ...input
-        }
+        };
 
-        return Tours[ToursIndex]
+        return Tours[ToursIndex];
     }
 }
+
+TourModel.init();
 
